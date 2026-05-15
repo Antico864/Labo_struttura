e5090190108dd7d80d5611159d99ad38e4e218ed
Antico864 <giovanni.bollotta@gmail.com>
2026-05-15 17:31:37 +0200

    Aggiornamento calcolo D_g, implementato in parte il trattamento degli errori.

--- analysis/functions.py
+++ analysis/functions.py
@@ -7,6 +7,7 @@
 # print(sys.executable)
 
 import numpy as np
+from datetime import datetime
 import math as math
 
 # interpolation
@@ -16,7 +17,7 @@ def chi2(meas_values, exp_values, err_on_exp_values):
     return sum((meas_values - exp_values)**2/err_on_exp_values)
 
 # Errore sistematico su R
-def sist_err_R(R_value):
+def instr_err_R(R_value):
     if R_value <= 2e2:
         return 1e-2
     elif R_value <= 2e3:
@@ -35,7 +36,7 @@ def sist_err_R(R_value):
         return np.inf
 
 # l'errore sistematico su T è costante
-def sist_err_T(T_value):
+def instr_err_T(T_value): # IN NANOMETRI
     return 0.1
 
 def quadsum(errors):
@@ -46,11 +47,24 @@ def media_mobile(values, errors, finestra = 11):
     avg_errors = np.sqrt(np.convolve(errors**2, np.ones(finestra), mode='valid')) / finestra
     return avg_values, avg_errors
 
-def assign_uncoupling_error(data):
-    R = data[:,0]
-    T = data[:,1]
-    R_times = np.str2float(data[:,2])
-    T_times = np.str2float(data[:,3])
+def string_to_seconds(stringa_tempo):
+    # decodifica se numpy passa la stringa in formato bytes (succede in vecchie versioni)
+    if isinstance(stringa_tempo, bytes):
+        stringa_tempo = stringa_tempo.decode('utf-8')
+        
+    t = datetime.strptime(stringa_tempo, "%H:%M:%S.%f")
+    # Calcola i secondi totali dalla mezzanotte
+    return t.hour * 3600 + t.minute * 60 + t.second + t.microsecond / 1e6
+
+def assign_uncoupling_error(filename):
+
+    R, T, R_times, T_times = np.loadtxt(
+        filename, 
+        skiprows=1,
+        converters={2: string_to_seconds, 3: string_to_seconds},
+        unpack=True
+    )
+    T = T*100 # in nm
 
     # Prima parte dei dati: indici per i quali R è OL
     ins_mask = (R>999999999)
@@ -60,8 +74,8 @@ def assign_uncoupling_error(data):
     # Qui non c'è problema
     R_values_ins = R_ins # tutti costanti, 1000000000
     T_values_ins = T_ins
-    R_err_ins = 0.0
-    T_err_ins = 0.0 # 0.0 perché è solo quello del disallineamento, che qui in pratica non c'è
+    R_err_ins = np.zeros_like(R_values_ins)
+    T_err_ins = np.zeros_like(T_values_ins) # 0.0 perché è solo quello del disallineamento, che qui in pratica non c'è
 
     # Seconda parte dei dati: tutto il resto
     cond_mask = (R<999999999)
@@ -83,12 +97,38 @@ def assign_uncoupling_error(data):
     T_err_c = 2*np.abs(T_values_c - T_c)
 
     # concatenali nei valori finali
-    R_values = np.concatenate(R_values_ins, R_values_c)
-    T_values = np.concatenate(T_values_ins, T_values_c)
-    R_err = np.concatenate(R_err_ins, R_err_c)
-    T_err = np.concatenate(T_err_ins, T_err_c)
+    R_values = np.concatenate((R_values_ins, R_values_c))
+    T_values = np.concatenate((T_values_ins, T_values_c))
+    R_err = np.concatenate((R_err_ins, R_err_c))
+    T_err = np.concatenate((T_err_ins, T_err_c))
 
     return R_values, R_err, T_values, T_err
 
 # utilizzo: 
-# R_values, R_err, T_values, T_err = assign_uncoupling_error(data)
+# R_values, R_err, T_values, T_err = assign_uncoupling_error('data/vetro_1_28_04.txt')
+
+# print(f"Riga n° 2086: \n {R_values[2086]}±{R_err[2086]}\t{T_values[2086]}±{T_err[2086]}")
+
+# Sembra funzioni...
+
+def assign_err_RT(R_values, R_err, T_values, T_err):
+    # calcola l'errore su RT a partire dai singoli propagando
+    ...
+
+def assign_error_RT2(R_values, R_err, T_values, T_err): 
+    ...
+
+
+# Queste due non le facciamo già quando usiamo il fit????
+
+def assign_error_ohmic_fit(boh):
+    # che assegni gli errori ai valori attesi per il fit ohmico
+    ...
+
+def assign_error_linear_fit(boh):
+    # che assegni gli errori ai valori attesi per il fit lineare
+    ...
+
+### Da fare: 
+# *. Continuare a fare gli errori, tipo di tutto; 
+# *. Interpretare la roba del grain size!!!!
